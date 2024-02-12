3a03e61271e33e78248f24ccfddd395cfae8e95b
mirko.viroli <mirko.viroli@unibo.it>
2024-02-12 08:57:23 +0100

    mockito and junit work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.*;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> listMock = mock(List.class);
+        when(listMock.size()).thenReturn(5);
+        System.out.println(listMock.size());
     }
 }
--- src/test/java/TestList.java
+++ src/test/java/TestList.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class TestList {
     @Test
-    public void listInsertionOrderShouldBePreserved() {
-        List<String> list = new ArrayList<>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        assertEquals(List.of("a", "b", "c"), list);
+    public void test() {
+        List<String> listMock = mock(List.class);
+        when(listMock.size()).thenReturn(5);
+        assertEquals(5, listMock.size());
+
+        listMock.add("5");
+        listMock.clear();
+
+        verify(listMock).clear();
+        verify(listMock).add("5");
+
     }
 }
